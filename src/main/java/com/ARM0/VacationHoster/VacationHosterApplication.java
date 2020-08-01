c4b80bfcc4552b520e514f4856a1f80ce1c37aa9
Sahibdeep Singh <ssahibdeep12@gmail.com>
2020-08-01 00:36:19 -0400

    First Draft

---
+++ src/main/java/com/ARM0/VacationHoster/VacationHosterApplication.java
@@ -0,0 +1,13 @@
+package com.ARM0.VacationHoster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationHosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationHosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ARM0/VacationHoster/VacationHosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ARM0.VacationHoster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationHosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
